ebda0814eee2af24ba9084ac83db3189a9c30514
qua3k <78624738+qua3k@users.noreply.github.com>
2021-07-18 10:54:11 +0000

    Delete jQuery

--- background.js
+++ background.js
@@ -1,12 +1,10 @@
-$(document).ready(() => {
-	let chance = Math.floor(Math.random() * 100);
-	if(chance === 69) {
-		rickrollify();
-	}
-})
-
 const rickrollify = () => {
-	setTimeout(() => { // Wait 3 seconds before rickrolling
-			window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1'
-		}, 3000)
+    setTimeout(() => { // Wait 3 seconds before rickrolling
+        location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1'
+    }, 3000)
 }
+
+const chance = Math.floor(Math.random() * 100);
+    if (chance === 69) {
+        rickrollify();
+    }
