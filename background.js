02ccd76479b2b0139a25a5a3ee7d5e2015cf3555
qua3k <78624738+qua3k@users.noreply.github.com>
2021-07-18 10:54:11 +0000

    Rewrite background.js

--- background.js
+++ background.js
@@ -1,22 +1,12 @@
-$(document).ready(function() {
-	var chance = Math.floor(Math.random() * 100);
-	if(chance == 69) {
+$(document).ready(() => {
+	let chance = Math.floor(Math.random() * 100);
+	if(chance === 69) {
 		rickrollify();
 	}
-});
+})
 
-var generateHTML = function() {
-	var html = "<video autoplay loop style='height: 100vh; width: 100vw'><source src='https://codeberg.org/duck/Rickrollify/raw/branch/master/rick.webm' type='video/webm'><source src='https://codeberg.org/duck/Rickrollify/raw/branch/master/rick.mp4' type='video/mp4'></video>";
-	return html;
-};
-
-var rickrollify = function() {
-	setTimeout(function() { // Wait 3 seconds before rickrolling
-		var html = generateHTML();
-		$('body').html(html);
-		document.body.style.backgroundColor = "black";
-		setTimeout(function() { // Wait 60 seconds for rickroll to finish
-			window.location.replace(document.URL);
-		}, 60000);
-	}, 3000);
-};
+const rickrollify = () => {
+	setTimeout(() => { // Wait 3 seconds before rickrolling
+			window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1'
+		}, 3000)
+}
